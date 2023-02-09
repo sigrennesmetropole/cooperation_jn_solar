adcedfe47ddf36698d3fc2354345d5d03ed4e9a4
Yohann Marchat <yohann.marchat@camptocamp.com>
2023-02-09 15:23:33 +0100

    refacto: adaptation to work with common ui

---
+++ src/components/ui/UiSolarPanelTitle.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import UiSolarPanelLogo from '@/components/ui/UiSolarPanelLogo.vue'
+import { UiAbstractTitle } from 'cooperation_jn_common_ui'
+const title = 'Cadastre Solaire'
+const abstract =
+  'Projetez-vous en tant que producteur d’électricité grâce à l’énergie solaire photovoltaïque.'
+</script>
+
+<template>
+  <UiAbstractTitle :logo="UiSolarPanelLogo" :abstract="abstract" :title="title">
+  </UiAbstractTitle>
+</template>
