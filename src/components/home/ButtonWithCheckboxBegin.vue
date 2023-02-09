c72bd89062b30c6b78b908d57fcde233799e39f2
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-02-09 15:40:46 +0100

    home : component button with checkbox begin

---
+++ src/components/home/ButtonWithCheckboxBegin.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const checked = ref(false)
+const displayError = ref(false)
+
+function openPopUpTermsOfUse() {}
+
+function clickButtonBegin() {
+  if (!checked.value) {
+    displayError.value = true
+    return
+  }
+  displayError.value = false
+}
+</script>
+
+<template>
+  <div class="flex p-0 gap-3">
+    <input
+      type="checkbox"
+      v-model="checked"
+      class="bg-white border-slate-400 shadow-sm rounded"
+    />
+    <span class="font-normal text-base leading-6 color-black">
+      J'ai lu et j'accepte les
+      <span
+        class="underline decoration-1 cursor-pointer"
+        @click="openPopUpTermsOfUse()"
+      >
+        conditions d'utilisation.
+      </span>
+    </span>
+  </div>
+
+  <div class="flex p-0 gap-3">
+    <button
+      @click="clickButtonBegin()"
+      class="bg-black shadow-sm rounded-lg gap-3 px-4 py-3 items-center flex flex-row justify-center"
+    >
+      <span class="text-white text-base font-bold"> Commencer </span>
+    </button>
+
+    <span v-if="displayError && !checked" class="text-sm font-normal">
+      Veuillez cocher la case ci-dessus pour accepter les conditions
+    </span>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import FooterArea from '@/components/home/FooterArea.vue'
+import ButtonWithCheckboxBegin from '@/components/home/ButtonWithCheckboxBegin.vue'
 </script>
 
 <template>
   HOME VIEW
+  <ButtonWithCheckboxBegin />
   <FooterArea></FooterArea>
 </template>
