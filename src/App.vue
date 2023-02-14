3ea1b753112c483e8b001defd6f65fddaedaa708
Ismail Sunni <imajimatika@gmail.com>
2023-02-14 13:56:46 +0700

    Make panel retractable on home view only.

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,26 @@ import SidePanel from '@/components/home/SidePanel.vue'
 import { RennesApp } from '@/services/RennesApp'
 import MapComponent from '@/components/map/MapComponent.vue'
 import mapConfig from './map.config.json'
+import { useViewsStore } from './stores/views'
+import { viewList } from './model/views.model'
+
+const viewStore = useViewsStore()
 
 onBeforeMount(() => {
   const rennesApp = new RennesApp(mapConfig)
   provide('rennesApp', rennesApp)
 })
+
+function isLeftPanelRetractable() {
+  const retractableList = [viewList['map-pcaet']]
+  return retractableList.includes(viewStore.currentView)
+}
 </script>
 
 <template>
   <main class="h-screen flex">
     <aside class="z-10 absolute">
-      <SidePanel :is-retractable="false">
+      <SidePanel :is-retractable="isLeftPanelRetractable()">
         <RouterView :key="$route.fullPath" />
       </SidePanel>
     </aside>
---
+++ src/views/MapPCAETView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { FooterArea } from 'cooperation_jn_common_ui'
+</script>
+
+<template>
+  Map PCAET
+  <FooterArea></FooterArea>
+</template>
