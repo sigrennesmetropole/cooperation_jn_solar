454d6d72837d8b1474b0597ac7b38628c1984947
Ismail Sunni <imajimatika@gmail.com>
2023-02-19 00:10:13 +0700

    Add slot for the figure energy content.

--- src/components/pcaet/FigureEnergy.vue
+++ src/components/pcaet/FigureEnergy.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import FigureKeyNumber from './FigureKeyNumber.vue'
+import HourGlass from '@/assets/illustrations/hour-glass.svg'
+import SolarPanel from '@/assets/illustrations/solar-panel.svg'
+import HousePlant from '@/assets/illustrations/house-plant.svg'
 </script>
 
 <template>
@@ -7,9 +10,37 @@ import FigureKeyNumber from './FigureKeyNumber.vue'
     <div
       class="flex flex-col p-6 gap-6 bg-slate-50 border-slate-100 border-[1px] rounded-lg"
     >
-      <FigureKeyNumber></FigureKeyNumber>
-      <FigureKeyNumber></FigureKeyNumber>
-      <FigureKeyNumber></FigureKeyNumber>
+      <FigureKeyNumber>
+        <template v-slot:image>
+          <img class="w-32 h-16 max-w-none" :src="HourGlass" alt="" />
+        </template>
+        <template v-slot:text>
+          <p class="font-dm-sans text-xs font-bold">À l’horizon</p>
+          <p class="font-dm-sans text-[44px] leading-10 font-bold">2030</p>
+        </template>
+      </FigureKeyNumber>
+      <FigureKeyNumber>
+        <template v-slot:image>
+          <img class="w-32 h-16 max-w-none" :src="HousePlant" alt="" />
+        </template>
+        <template v-slot:text>
+          <p class="font-dm-sans text-3xl font-bold">3x</p>
+          <p class="font-dm-sans text-xs font-bold">
+            plus d’énergies renouvelables
+          </p>
+        </template>
+      </FigureKeyNumber>
+      <FigureKeyNumber>
+        <template v-slot:image>
+          <img class="w-32 h-16 max-w-none" :src="SolarPanel" alt="" />
+        </template>
+        <template v-slot:text>
+          <p class="font-dm-sans text-3xl font-bold">6x</p>
+          <p class="font-dm-sans text-xs font-bold">
+            plus de puissance photovoltaïque
+          </p>
+        </template>
+      </FigureKeyNumber>
     </div>
   </div>
 </template>
--- src/components/pcaet/FigureKeyNumber.vue
+++ src/components/pcaet/FigureKeyNumber.vue
@@ -1,15 +1,12 @@
-<script setup lang="ts">
-import HourGlass from '@/assets/illustrations/hour-glass.svg'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="flex flex-row items-start p-0 gap-[18px]">
     <div class="flex flex-col items-start p-0 gap-6">
       <div class="flex flex-row items-center p-0 gap-6">
-        <img class="w-32 h-16 max-w-none" :src="HourGlass" alt="" />
-        <div class="flex flex-col justify-center items-start p-0">
-          <p class="font-dm-sans text-xs font-bold">À l’horizon</p>
-          <p class="font-dm-sans text-[44px] leading-10 font-bold">2030</p>
+        <slot name="image"></slot>
+        <div class="flex flex-col justify-center items-start p-0 w-32">
+          <slot name="text"></slot>
         </div>
       </div>
     </div>
