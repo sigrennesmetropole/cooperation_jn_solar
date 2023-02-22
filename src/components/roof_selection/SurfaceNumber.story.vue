1110de41a4c64667ae387eaf6471c72cb4dffef5
Ismail Sunni <imajimatika@gmail.com>
2023-02-22 17:49:20 +0700

    Add Surface number component with static value.

---
+++ src/components/roof_selection/SurfaceNumber.story.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import SurfaceNumber from '@/components/roof_selection/SurfaceNumber.vue'
+</script>
+
+<template>
+  <Story title="Surface Number" group="pcaetpage">
+    <template #default>
+      <SurfaceNumber class="w-[402px] h-[100px]"> </SurfaceNumber>
+    </template>
+  </Story>
+</template>
---
+++ src/components/roof_selection/SurfaceNumber.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import RightFlatArrow from '@/assets/illustrations/right-flat-arrow.svg'
+</script>
+
+<template>
+  <div class="flex flex-row p-4 gap-4 bg-slate-50 rounded-br-lg rounded-bl-lg">
+    <div class="flex flex-col p-0">
+      <h2 class="font-dm-sans font-bold text-2xl text-center">90 m&sup2;</h2>
+      <p class="font-dm-sans font-normal text-sm text-neutral-700 text-center">
+        Surface de toiture du bâtiment
+      </p>
+    </div>
+    <img :src="RightFlatArrow" />
+    <div>
+      <h2 class="font-dm-sans font-bold text-2xl text-center">44 m&sup2;</h2>
+      <p class="font-dm-sans font-normal text-sm text-neutral-700 text-center">
+        Surface favorable au photovoltaïque
+      </p>
+    </div>
+  </div>
+</template>
