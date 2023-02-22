2221c7e1ed472a805d6ff3d9e16aaa684d204495
Ismail Sunni <imajimatika@gmail.com>
2023-02-22 22:24:11 +0700

    Add prop for number.

--- src/components/roof_selection/SurfaceNumber.story.vue
+++ src/components/roof_selection/SurfaceNumber.story.vue
@@ -5,7 +5,12 @@ import SurfaceNumber from '@/components/roof_selection/SurfaceNumber.vue'
 <template>
   <Story title="Surface Number" group="pcaetpage">
     <template #default>
-      <SurfaceNumber class="w-[402px] h-[100px]"> </SurfaceNumber>
+      <SurfaceNumber
+        class="w-[402px] h-[100px]"
+        :allArea="90"
+        :favorableArea="44"
+      >
+      </SurfaceNumber>
     </template>
   </Story>
 </template>
--- src/components/roof_selection/SurfaceNumber.vue
+++ src/components/roof_selection/SurfaceNumber.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 import RightFlatArrow from '@/assets/illustrations/right-flat-arrow.svg'
+
+defineProps({
+  allArea: {
+    type: Number,
+    required: true,
+  },
+  favorableArea: {
+    type: Number,
+    required: true,
+  },
+})
 </script>
 
 <template>
   <div class="flex flex-row p-4 gap-4 bg-slate-50 rounded-br-lg rounded-bl-lg">
     <div class="flex flex-col p-0">
-      <h2 class="font-dm-sans font-bold text-2xl text-center">90 m&sup2;</h2>
+      <h2 class="font-dm-sans font-bold text-2xl text-center">
+        {{ allArea }} m&sup2;
+      </h2>
       <p class="font-dm-sans font-normal text-sm text-neutral-700 text-center">
         Surface de toiture du bâtiment
       </p>
     </div>
     <img :src="RightFlatArrow" />
     <div>
-      <h2 class="font-dm-sans font-bold text-2xl text-center">44 m&sup2;</h2>
+      <h2 class="font-dm-sans font-bold text-2xl text-center">
+        {{ favorableArea }} m&sup2;
+      </h2>
       <p class="font-dm-sans font-normal text-sm text-neutral-700 text-center">
         Surface favorable au photovoltaïque
       </p>
