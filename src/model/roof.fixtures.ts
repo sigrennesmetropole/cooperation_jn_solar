5d9265f769c863f88a0469accc5e4269947c58ab
Ismail Sunni <imajimatika@gmail.com>
2023-03-06 17:40:01 +0700

    Update roof fixtures.

--- src/model/roof.fixtures.ts
+++ src/model/roof.fixtures.ts
@@ -9,10 +9,10 @@ export const roofSurfaceFixtures = (): RoofSurfaceModel[] => [
     incliniasion: 47.9,
   },
   {
-    values: [10, 20, 30, 40],
-    favorable: 44,
-    total: 90,
-    orientation: 'Sud',
-    incliniasion: 47.9,
+    values: [40, 30, 20, 10],
+    favorable: 30,
+    total: 87,
+    orientation: 'Nord',
+    incliniasion: 49,
   },
 ]
