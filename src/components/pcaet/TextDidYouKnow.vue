6075d35798e46e0e917ee7a803389af2cca95bfb
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-03-08 23:02:32 +0100

    text did you know: add icon at the right of link

--- src/components/pcaet/TextDidYouKnow.vue
+++ src/components/pcaet/TextDidYouKnow.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import iconExternalLink from '@/assets/icons/icon-external-link.svg'
+</script>
 
 <template>
-  <div class="flex flex-col gap-3">
-    <h1 class="mt-5 font-dm-sans font-bold text-xl leading-7 text-black">
+  <div>
+    <h1 class="mt-5 mb-3 font-dm-sans font-bold text-xl leading-7 text-black">
       Le saviez-vous ?
     </h1>
     <p class="font-dm-sans font-normal text-sm text-black">
@@ -10,10 +12,12 @@
       <a
         href="https://metropole.rennes.fr/le-plan-climat-de-rennes-metropole"
         target="_blank"
-        class="underline"
+        class="underline inline-flex items-center"
       >
-        <strong> Plan Climat Air Energie Territorial (PCAET) </strong>
+        <strong> Plan Climat Air Energie Territorial (PCAET) &nbsp;</strong>
+        <img :src="iconExternalLink" class="w-[10px] h-[10px] text-undeline" />
       </a>
+
       marque l'engagement du territoire de Rennes Metropole dans la lutte contre
       le réchauffement climatique et la transition vers un territoire
       "post-carbone".
