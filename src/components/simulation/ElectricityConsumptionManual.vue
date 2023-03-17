fb83d8e9d819a75992447bf743fb1d0b32b9c418
anaislet-c2c <anais.letellier@camptocamp.com>
2023-03-17 11:49:53 +0100

    352- fixed after:content

--- src/components/simulation/ElectricityConsumptionManual.vue
+++ src/components/simulation/ElectricityConsumptionManual.vue
@@ -11,12 +11,18 @@ const storeConsumption = () => {
 <template>
   <div class="flex flex-col gap-[6px]">
     <p class="font-dm-sans text-sm">Votre consommation annuelle</p>
-    <input
-      class="border border-neutral-400 rounded h-[42px] px-3 py-[9px] text-black font-dm-sans text-medium placeholder:text-neutral-600 placeholder:text-base placeholder:font-dm-sans"
-      type="text"
-      placeholder="kWh"
-      v-model="consumption"
-      @keyup="storeConsumption"
-    />
+    <div class="flex flex-row">
+      <input
+        class="border border-neutral-400 rounded h-[42px] px-3 py-[9px] text-black font-dm-sans text-medium placeholder:text-neutral-600 placeholder:text-base placeholder:font-dm-sans w-full"
+        type="number"
+        placeholder="kWh"
+        v-model="consumption"
+        @keyup="storeConsumption"
+      />
+      <div
+        append-unit="kWh"
+        class="text-base font-dm-sans text-medium relative after:content-[attr(append-unit)] after:absolute after:right-2 after:top-2"
+      ></div>
+    </div>
   </div>
 </template>
