1e29244cf274e02492b79f8d8dae754184139fa1
Ismail Sunni <imajimatika@gmail.com>
2023-03-20 09:19:49 +0700

    Add more step description based on the current step and substep.

--- src/components/simulation/StepDescription.vue
+++ src/components/simulation/StepDescription.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-
 const props = defineProps({
   step: {
     type: Number,
     required: true,
   },
-  // Used for installation step that has 2 substep
   substep: {
     type: Number,
     required: false,
@@ -14,33 +11,28 @@ const props = defineProps({
   },
 })
 
-const mainDescriptions = [
-  'Je sélectionne le meilleur pan de toit.',
-  'J’identifie les obstacles sur ma toiture.',
-  'Estimez quelle sera votre production, quelle part vous consommerez directement (en fonction de votre profil de consommation) et quelle part sera injectée sur le réseau.',
-]
-
-const secondaryDescriptions = [
-  null,
-  'Mon installation photovoltaique simulée :',
-  null,
-]
-
-const getDescription = computed(() => {
-  if (props.substep === 1) {
-    return mainDescriptions[props.step - 1]
-  } else {
-    return (
-      secondaryDescriptions[props.step - 1] ?? mainDescriptions[props.step - 1]
-    )
-  }
-})
+// Text description according to step and substep
+const descriptions: Record<number, Record<number, string>> = {
+  1: {
+    1: 'Je sélectionne le meilleur pan de toit.',
+  },
+  2: {
+    1: 'J’identifie les obstacles sur ma toiture.',
+    2: 'Mon installation photovoltaique simulée :',
+  },
+  3: {
+    1: 'Estimez quelle sera votre production, quelle part vous consommerez directement (en fonction de votre profil de consommation) et quelle part sera injectée sur le réseau.',
+    2: 'Renseignez votre consommation électrique annuelle',
+    3: '',
+    4: '',
+  },
+}
 </script>
 <template>
   <p
     class="font-dm-sans text-base text-center"
     :class="props.step == 3 ? 'font-normal' : 'font-medium'"
   >
-    {{ getDescription }}
+    {{ descriptions[props.step][props.substep] }}
   </p>
 </template>
