4b3761c01d3689ff958e178eb52e1195fa113025
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-03-21 14:09:14 +0100

    Simulation step 4: result page header

---
+++ src/components/simulation/HeaderEndSimulation.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import UiSolarPanelLogo from '@/components/ui/UiSolarPanelLogo.vue'
+import UiHeaderFullScreen from '@/components/ui/UiHeaderFullScreen.vue'
+import iconHome from '@/assets/icons/icon-home.svg'
+</script>
+
+<template>
+  <UiHeaderFullScreen :urlBack="'/step-sunshine'">
+    <template v-slot:title-img>
+      <div class="flex items-center p-0 gap-2.5 ml-5">
+        <component
+          :is="UiSolarPanelLogo"
+          role="img"
+          class="h-10 w-10"
+        ></component>
+        <h1 class="font-bold text-2xl text-center">Cadastre Solaire</h1>
+      </div>
+    </template>
+    <template v-slot:button-content>
+      <img :src="iconHome" />
+      <span class="text-base font-medium">Retour à l'accueil</span>
+    </template>
+  </UiHeaderFullScreen>
+</template>
