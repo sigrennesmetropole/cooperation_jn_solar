a4e9f4f453243265a3b93f2a15a430e3295fce9b
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-03-24 15:49:21 +0100

    Statistics at a building level: link  numbers component to data

--- src/components/roof_selection/SurfaceNumber.vue
+++ src/components/roof_selection/SurfaceNumber.vue
@@ -2,18 +2,27 @@
 import SurfaceChartDonut from '@/components/chart/SurfaceChartDonut.vue'
 import UiSurfaceNumber from '@/components/roof_selection/UiSurfaceNumber.vue'
 import type { RoofSurfaceModel } from '@/model/roof.model'
+import { useRoofDataStore } from '@/stores/roofData'
 
-const props = defineProps<{
-  roofSurface: RoofSurfaceModel
-}>()
+const roofDataStore = useRoofDataStore()
+
+const roofSurface: RoofSurfaceModel = {
+  values: [45, 5, 15, 35],
+  favorable: 44,
+  total: 90,
+  orientation: 'sud',
+  inclinaison: 47,
+}
 </script>
 
 <template>
   <div class="flex flex-row items-center justify-center">
-    <SurfaceChartDonut :roofSurface="props.roofSurface"></SurfaceChartDonut>
+    <SurfaceChartDonut :roofSurface="roofSurface"></SurfaceChartDonut>
   </div>
-  <UiSurfaceNumber
-    :totalArea="props.roofSurface.total"
-    :favorableArea="props.roofSurface.favorable"
-  ></UiSurfaceNumber>
+  <template v-if="buildingSurface !== null">
+    <UiSurfaceNumber
+      :totalArea="roofDataStore.buildingData.total"
+      :favorableArea="roofDataStore.buildingData.favorable"
+    ></UiSurfaceNumber>
+  </template>
 </template>
