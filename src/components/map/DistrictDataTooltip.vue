a3cda30203241b6998c68ea79dbe3b9baa64df1f
anaislet-c2c <anais.letellier@camptocamp.com>
2023-03-31 11:41:52 +0200

    add dynamic district informations

--- src/components/map/DistrictDataTooltip.vue
+++ src/components/map/DistrictDataTooltip.vue
@@ -8,6 +8,7 @@ function closeTooltip() {
 }
 
 const state = reactive({
+  irisName: '' as string,
   totalConsumption: 0 as number,
   totalProduction: 0 as number,
   nbPhotovoltaicInstallation: 0 as number,
@@ -17,36 +18,48 @@ async function gettingDistrictDatas(codeIris: number) {
   const districtDatas = await apiEnedisDistrictService.getDistrictDatas(
     codeIris
   )
+  state.irisName = districtDatas.irisName
   state.totalConsumption = districtDatas.totalConsumption
   state.totalProduction = districtDatas.totalProduction
   state.nbPhotovoltaicInstallation = districtDatas.totalPhotovoltaicSites
-
-  console.log('district data', districtDatas)
 }
 
 onMounted(() => {
   console.log('chargement page')
-  gettingDistrictDatas(351890000)
+  gettingDistrictDatas(350650000)
 })
 
-function decomposeNumber(float: number) {
-  const roundFloat = float.toFixed(2)
+function removePartOfIrisName(irisName: string) {
+  const toRemove = '(commune non irisée)'
+  const correctedIrisName = irisName.replace(toRemove, '')
+  return correctedIrisName
+}
+
+function decomposeNumber(float: number, numberOfDecimals: number) {
+  const roundFloat = keepDecimals(float, numberOfDecimals)
   const numberInSting = roundFloat.toString()
   const splitNumber = numberInSting.split('.')
   return splitNumber
+}
+
+function keepDecimals(float: number, numberOfDecimals: number) {
+  const roundFloat = float.toFixed(numberOfDecimals)
+  return roundFloat
 }
 </script>
 
 <template>
   <div
-    class="w-[440px] h-[176px] transition-[height] absolute right-40 top-1/4 bg-white flex flex-col p-5 gap-3 rounded-lg"
+    class="min-w-[440px] transition-[height] absolute right-40 top-1/4 bg-white flex flex-col p-5 gap-3 rounded-lg"
   >
     <div class="flex flex-row justify-between">
-      <h2 class="font-dm-sans font-bold text-2xl">Nom du quartier IRIS</h2>
+      <h2 class="font-dm-sans font-bold text-2xl max-w-[420px]">
+        {{ removePartOfIrisName(state.irisName) }}
+      </h2>
       <img
         :src="iconDelete"
         alt=""
-        class="w-4 self-start"
+        class="w-4 self-start ml-2"
         @click="closeTooltip()"
       />
     </div>
@@ -54,29 +67,31 @@ function decomposeNumber(float: number) {
       class="bg-slate-50 border border-slate-200 rounded-lg p-3 flex flex-row gap-4 font-dm-sans"
     >
       <div>
-        <p class="font-medium text-xs">Nombre d'installation</p>
+        <p class="font-medium text-xs">Nombre<br />d'installation</p>
         <p class="font-bold text-3xl">{{ state.nbPhotovoltaicInstallation }}</p>
       </div>
       <div class="border border-neutral-300"></div>
       <div>
-        <p class="font-medium text-xs">Production photovoltaïque</p>
+        <p class="font-medium text-xs">Production<br />photovoltaïque</p>
         <div class="flex flex-row items-baseline font-bold">
           <p class="text-lg leading-6">
             <span class="text-3xl">{{
-              decomposeNumber(state.totalProduction)[0]
+              decomposeNumber(state.totalProduction, 1)[0]
             }}</span
-            >.{{ decomposeNumber(state.totalProduction)[1] }} MWh
+            >.{{ decomposeNumber(state.totalProduction, 1)[1] }} MWh
           </p>
         </div>
       </div>
       <div class="border border-neutral-300"></div>
       <div>
-        <p class="font-medium text-xs">Consommation annuelle</p>
+        <p class="font-medium text-xs">Consommation<br />annuelle</p>
         <div class="flex flex-row items-baseline font-bold">
-          <span class="text-3xl">{{
-            decomposeNumber(state.totalConsumption)[0]
-          }}</span
-          >.{{ decomposeNumber(state.totalConsumption)[1] }} MWh
+          <p>
+            <span class="text-3xl">{{
+              decomposeNumber(state.totalConsumption, 2)[0]
+            }}</span>
+            MWh
+          </p>
         </div>
       </div>
     </div>
