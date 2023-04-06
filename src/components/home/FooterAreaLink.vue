36553fcf2716f31b066995033425f22e28ef5d02
Yohann Marchat <yohann.marchat@camptocamp.com>
2023-04-06 11:02:59 +0200

    feat: add cookies page

--- src/components/home/FooterAreaLink.vue
+++ src/components/home/FooterAreaLink.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { FooterArea } from '@sigrennesmetropole/cooperation_jn_common_ui'
-
-const openLink = (link: String) => {
-  window.open('/legalnotice/' + link, '_blank')
+import { legalList } from '@/constants/legalLinks'
+const openLink = (link: string) => {
+  window.open(link, '_blank')
 }
 </script>
 
 <template>
-  <FooterArea class="mt-auto" @openLink="openLink($event)"></FooterArea>
+  <FooterArea
+    class="mt-auto"
+    @openLink="openLink($event)"
+    :legalList="legalList"
+  ></FooterArea>
 </template>
---
+++ src/constants/legalLinks.ts
@@ -0,0 +1,29 @@
+import type { LegalItem } from '@sigrennesmetropole/cooperation_jn_common_ui/dist/model/legalItem.model'
+
+export const legalList: LegalItem[] = [
+  {
+    link: '/legalnotice/mentions-legales',
+    slug: 'mentions-legales',
+    name: 'Mentions légales',
+  },
+  {
+    link: '/legalnotice/confidentialite',
+    slug: 'confidentialite',
+    name: 'Politique de confidentialité',
+  },
+  {
+    link: '/legalnotice/plan-du-site',
+    slug: 'plan-du-site',
+    name: 'Plan du site',
+  },
+  {
+    link: '/legalnotice/accessibilite',
+    slug: 'accessibilite',
+    name: 'Accessibilité: totalement conforme',
+  },
+  {
+    link: '/legalnotice/cookies',
+    slug: 'cookies',
+    name: 'Cookies',
+  },
+]
