d3ddb1bc230e3e6fdbe260b2690dbac1f38589aa
Ismail Sunni <imajimatika@gmail.com>
2023-04-12 15:58:22 +0700

    Add unified solar placement function.

--- src/algorithm/__tests__/solarPanelPlacement.spec.ts
+++ src/algorithm/__tests__/solarPanelPlacement.spec.ts
@@ -1,7 +1,10 @@
 import { assert, describe, expect, it } from 'vitest'
 import * as fs from 'fs'
 import * as path from 'path'
-import { solarPanelPlacement } from '../solarPanelPlacement'
+import {
+  solarPanelPlacementAlgorithm,
+  solarPanelPlacement,
+} from '../solarPanelPlacement'
 import type { FeatureCollection, Polygon, Properties } from '@turf/turf'
 
 describe('solar panel placement', () => {
@@ -16,8 +19,23 @@ describe('solar panel placement', () => {
 
     const grid: FeatureCollection<Polygon, Properties> =
       JSON.parse(fileContents)
-    solarPanelPlacement(grid, true)
-    solarPanelPlacement(grid, false)
+
+    const horizontalSolarPanels = solarPanelPlacementAlgorithm(
+      grid,
+      true,
+      false
+    )
+    expect(horizontalSolarPanels.features.length).greaterThanOrEqual(9)
+
+    const verticalSolarPanels = solarPanelPlacementAlgorithm(grid, false, false)
+    // In this test case, the vertical placement is not optimum, only 8
+    expect(verticalSolarPanels.features.length).greaterThanOrEqual(8)
+
+    const solarPanelsPlacement = solarPanelPlacement(grid)
+    expect(solarPanelsPlacement.solarPanels.features.length).greaterThanOrEqual(
+      9
+    )
+    expect(solarPanelsPlacement.orientation).equal('horizontal')
 
     assert.equal(Math.sqrt(4), 2)
   })
--- src/algorithm/solarPanelPlacement.ts
+++ src/algorithm/solarPanelPlacement.ts
@@ -3,17 +3,25 @@
 //  - grid
 
 // ALgorithm
-// Create a multipolygon from all the grids, called it possible rectangle
+// Create a multipolygon from all the grids, called it allGrid
 
 // For each grid, create a rectangle that represent a solar panel which share the top-left corner with the grid
-// Check if the solar panel is inside the _possible rectangle_
-// If not, remove it
-// If yes, store it as possible solar panel
+// - Make the grid 4x bigger
+// - Cut it by half, depending on whether it's vertical or horizontal
+// - Add inside_valid_roof and not_covered properties, set them to True
 
-// For each possible solar panel, get all intersected possible solar panel
-// - If there is a solar panel that has 0 intersection, put it in the final solar panel
+// Check if the solar panel is inside the allGrid
+// If not, remove it / update the inside_valid_roof to false
+// If yes, update the inside_valid_roof to true
 
-// OPTIONS 1
+// OPTIONS 1 (current implementation)
+// For each solar panel
+// - if it's inside_valid_roof and not_covered, check to other solar panel
+// - Mark all solar panel covered by this solar panel by set not_covered = False
+// Get all solar panel that is not_covered and inside_valid_roof as the result
+
+// OPTIONS 2
+// Create a graph that represent the intersection between solar panel
 // While the number of intersected possible solar panel > 0
 // Remove the possible solar panel that has the biggest number of intersection
 // Update the graph
@@ -42,12 +50,15 @@ import * as path from 'path'
 
 function writeFeature(
   outputFileName: string,
-  feature: FeatureCollection | Feature
+  feature: FeatureCollection | Feature,
+  debug: boolean = false
 ) {
-  fs.writeFileSync(
-    path.join(__dirname, outputFileName),
-    JSON.stringify(feature)
-  )
+  if (debug) {
+    fs.writeFileSync(
+      path.join(__dirname, outputFileName),
+      JSON.stringify(feature)
+    )
+  }
 }
 
 function createSolarPanel(
@@ -120,11 +131,11 @@ function createSolarPanel(
   return fullSolarPanel
 }
 
-export function solarPanelPlacement(
+export function solarPanelPlacementAlgorithm(
   grid: FeatureCollection<Polygon, Properties>,
-  horizontal: boolean = true
+  horizontal: boolean = true,
+  debug: boolean = false
 ) {
-  console.log('solarPanelPlacement algorithm')
   const prefix = horizontal ? 'h-' : 'v-'
 
   let allGrid: Feature<Polygon | MultiPolygon, Properties> | null =
@@ -147,22 +158,25 @@ export function solarPanelPlacement(
     }
   })
 
-  writeFeature(`./${prefix}allGrid.geojson`, allGrid)
-  writeFeature(`./${prefix}solarPanelsFromGrid.geojson`, solarPanels)
+  writeFeature(`./${prefix}allGrid.geojson`, allGrid, debug)
+  writeFeature(`./${prefix}solarPanelsFromGrid.geojson`, solarPanels, debug)
 
   // Mark the solar panel that are not inside the selected roof / grid
+  // TODO (IS): Future improvement, remove the solar panel that is not inside the allGrid
   featureEach(solarPanels, (cf, _fi) => {
     cf.properties!['inside_valid_roof'] = booleanWithin(
+      // TODO (IS): Future improvement, put the scaling when we create solarPanel
       transformScale(cf, 0.9),
       allGrid!
     )
   })
-  writeFeature(`./${prefix}solarPanelsInsideRoof.geojson`, solarPanels)
+  writeFeature(`./${prefix}solarPanelsInsideRoof.geojson`, solarPanels, debug)
 
   // Mark solar panel that is not covered by other solar panel
-  // Starting from the first solar panel first (brute force)
+  // Starting from the first solar panel first (greedy approach)
   featureEach(solarPanels, (cf, fi) => {
     if (cf.properties!['inside_valid_roof'] && cf.properties!['not_covered']) {
+      // TODO (IS): Future improvement, do not use featureEach, but use filter (turf doesn't have this)
       featureEach(solarPanels, (cf2, fi2) => {
         if (fi < fi2) {
           if (booleanIntersects(transformScale(cf, 0.9), cf2)) {
@@ -172,5 +186,35 @@ export function solarPanelPlacement(
       })
     }
   })
-  writeFeature(`./${prefix}solarPanelsCheckCoverage.geojson`, solarPanels)
+  writeFeature(
+    `./${prefix}solarPanelsCheckCoverage.geojson`,
+    solarPanels,
+    debug
+  )
+  // TODO (IS): Future improvement, remove the unnecessary properties
+  solarPanels.features = solarPanels.features.filter((f) => {
+    return f.properties!['inside_valid_roof'] && f.properties!['not_covered']
+  })
+
+  writeFeature(`./${prefix}finalSolarPanels.geojson`, solarPanels, debug)
+
+  return solarPanels
+}
+
+export function solarPanelPlacement(
+  grid: FeatureCollection<Polygon, Properties>
+) {
+  const verticalSolarPanels = solarPanelPlacementAlgorithm(grid, false)
+  const horizontalSolarPanels = solarPanelPlacementAlgorithm(grid, true)
+
+  if (
+    horizontalSolarPanels.features.length >= verticalSolarPanels.features.length
+  ) {
+    return {
+      solarPanels: horizontalSolarPanels,
+      orientation: 'horizontal',
+    }
+  } else {
+    return { solarPanels: verticalSolarPanels, orientation: 'vertical' }
+  }
 }
