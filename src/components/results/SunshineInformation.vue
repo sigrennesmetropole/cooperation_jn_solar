cf0cb770f9669827bd7c8cda394e5a4fed7a8f8c
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-04-20 15:34:03 +0200

    final result : SunshineInformation

---
+++ src/components/results/SunshineInformation.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import potentiel from '@/assets/icons/potentiel.svg'
+import { useRoofsStore } from '@/stores/roof'
+import { useAddressStore } from '@/stores/address'
+
+import RoofInformationsBox from '@/components/simulation/RoofInformationsBox.vue'
+
+const addressStore = useAddressStore()
+const roofsStore = useRoofsStore()
+
+const address = addressStore.address
+
+let selectedRoof = roofsStore.getRoofSurfaceModelOfSelectedPanRoof()
+//TODO : remove this line when not testing
+selectedRoof = {
+  surface_id: 53123,
+  values: [13.832112, 74.012886, 12.155002, 0],
+  favorable: 10,
+  total: 87,
+  orientation: 'EST',
+  azimuth: 80,
+  inclinaison: 1,
+}
+</script>
+
+<template>
+  <div
+    class="flex flex-col gap-3 w-full h-fit bg-white rounded-xl p-6 mx-auto shadow-md font-dm-sans"
+  >
+    <div class="flex flex-row justify-center items-center gap-2">
+      <img :src="potentiel" alt="" class="w-11 h-10" />
+      <span class="font-bold text-2xl"> Votre ensoleillement </span>
+    </div>
+
+    <div class="px-2 mt-2">
+      <div class="font-normal text-xs mb-4">
+        {{ address }}
+      </div>
+
+      <RoofInformationsBox :roofSurface="selectedRoof" :isDisplayBox="false" />
+    </div>
+  </div>
+</template>
--- src/views/SimulationResultsView.vue
+++ src/views/SimulationResultsView.vue
@@ -1,23 +1,36 @@
 <script setup lang="ts">
-import { onBeforeMount, onMounted } from 'vue'
+import { onBeforeMount } from 'vue'
 import { viewList } from '@/model/views.model'
 import { useViewsStore } from '@/stores/views'
+import { useConsumptionAndProductionStore } from '@/stores/consumptionAndProduction'
 import ConsumptionInformation from '@/components/results/ConsumptionInformation.vue'
+import SunshineInformation from '@/components/results/SunshineInformation.vue'
+import { useAddressStore } from '@/stores/address'
 
+const consumptionAndProductionStore = useConsumptionAndProductionStore()
+const addressStore = useAddressStore()
 const viewStore = useViewsStore()
 
 onBeforeMount(async () => {
   viewStore.setCurrentView(viewList['simulation-results'])
+
+  //TODO : remove these lines when not testing
+  consumptionAndProductionStore.setAnnualConsumption(6000)
+  addressStore.setAddress('17, rue de la Finlande, 35000 Rennes')
 })
-onMounted(async () => {})
 </script>
 
 <template>
-  <div class="flex flex-row mx-auto mt-[184px] gap-6 w-[80%] justify-center">
-    <div class="w-[25%] font-dm-sans font-medium flex flex-col gap-6">
-      <ConsumptionInformation></ConsumptionInformation>
+  <div class="flex flex-row mx-auto mt-[184px] w-full gap-6 justify-center">
+    <div
+      class="w-[25%] max-w-[360px] font-dm-sans font-medium flex flex-col gap-6"
+    >
+      <SunshineInformation />
+      <ConsumptionInformation />
     </div>
-    <div class="w-[55%] font-dm-sans font-medium flex flex-col gap-8">
+    <div
+      class="w-[55%] font-dm-sans font-medium flex flex-col gap-8 bg-blue-50"
+    >
       <div></div>
     </div>
   </div>
