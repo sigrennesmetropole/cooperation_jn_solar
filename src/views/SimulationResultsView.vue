f6c4a6958e2a0e9ccf2c51b801c4291122d2f057
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-04-21 10:14:03 +0200

    fake data for test

--- src/views/SimulationResultsView.vue
+++ src/views/SimulationResultsView.vue
@@ -2,42 +2,42 @@
 import { onBeforeMount } from 'vue'
 import { viewList } from '@/model/views.model'
 import { useViewsStore } from '@/stores/views'
-import { useRoofsStore } from '@/stores/roof'
+// import { useRoofsStore } from '@/stores/roof'
 import ConsumptionInformation from '@/components/results/ConsumptionInformation.vue'
 import SunshineInformation from '@/components/results/SunshineInformation.vue'
 import AutocalsolResult from '@/components/results/AutocalsolResult.vue'
 import { useAutocalsolStore } from '@/stores/autocalsol'
 
 const viewStore = useViewsStore()
-const roofsStore = useRoofsStore()
+// const roofsStore = useRoofsStore()
 const autocalsolStore = useAutocalsolStore()
 
 onBeforeMount(async () => {
   viewStore.setCurrentView(viewList['simulation-results'])
 })
 
-const selectedRoof = roofsStore.getRoofSurfaceModelOfSelectedPanRoof()
-const autocalsolResult = autocalsolStore.autocalsolResult
+// const selectedRoof = roofsStore.getRoofSurfaceModelOfSelectedPanRoof()
+// const autocalsolResult = autocalsolStore.autocalsolResult
 
 //TO TEST : remove comment bellow | TODO : remove for production
-// import { useConsumptionAndProductionStore } from '@/stores/consumptionAndProduction'
-// const consumptionAndProductionStore = useConsumptionAndProductionStore()
-// import { useAddressStore } from '@/stores/address'
-// const addressStore = useAddressStore()
-// import autocalsolResultExample from '@/tests/stores/autocalsolResultExample.json'
-// autocalsolStore.setAutocalsolResult(autocalsolResultExample)
-// const autocalsolResult = autocalsolStore.autocalsolResult
-// consumptionAndProductionStore.setAnnualConsumption(6000)
-// addressStore.setAddress('17, rue de la Finlande, 35000 Rennes')
-// const selectedRoof = {
-//   surface_id: 53123,
-//   values: [13.832112, 74.012886, 12.155002, 0],
-//   favorable: 10,
-//   total: 87,
-//   orientation: 'EST',
-//   azimuth: 80,
-//   inclinaison: 1,
-// }
+import { useConsumptionAndProductionStore } from '@/stores/consumptionAndProduction'
+const consumptionAndProductionStore = useConsumptionAndProductionStore()
+import { useAddressStore } from '@/stores/address'
+const addressStore = useAddressStore()
+import autocalsolResultExample from '@/tests/stores/autocalsolResultExample.json'
+autocalsolStore.setAutocalsolResult(autocalsolResultExample)
+const autocalsolResult = autocalsolStore.autocalsolResult
+consumptionAndProductionStore.setAnnualConsumption(6000)
+addressStore.setAddress('17, rue de la Finlande, 35000 Rennes')
+const selectedRoof = {
+  surface_id: 53123,
+  values: [13.832112, 74.012886, 12.155002, 0],
+  favorable: 10,
+  total: 87,
+  orientation: 'EST',
+  azimuth: 80,
+  inclinaison: 1,
+}
 </script>
 
 <template>
