1eb4bbcb63122dec17b5ca0934771c6e7167a2b8
Yohann Marchat <yohann.marchat@camptocamp.com>
2023-04-27 10:22:20 +0200

    tests: add test

---
+++ src/tests/components/map/MapComponent.spec.ts
@@ -0,0 +1,114 @@
+import { describe, it, expect, vi } from 'vitest'
+import MapComponent from '@/components/map/MapComponent.vue'
+import { mount } from '@vue/test-utils'
+import { RennesApp } from '@/services/RennesApp'
+import { createTestingPinia } from '@pinia/testing'
+import SelectRoofInteraction from '@/interactions/selectRoofInteraction'
+import ForbidenClickInteraction from '@/interactions/forbidClickInteraction'
+import SelectDistrictInteraction from '@/interactions/selectDistrictInteractions'
+import { RENNES_LAYER } from '@/stores/layers'
+import { viewList } from '@/model/views.model'
+import { useRoofsStore } from '@/stores/roof'
+import { Context, GeoJSONLayer, OpenlayersMap } from '@vcmap/core'
+import type { GeoJSONFeatureCollection } from 'ol/format/GeoJSON'
+
+vi.mock('vue-router')
+vi.mock('@/services/roofInteractionHelper')
+
+describe('#MapComponent', async () => {
+  const olMap = new OpenlayersMap({ name: 'ol' }).toJSON()
+  const roofLayer: GeoJSONLayer = new GeoJSONLayer({
+    name: RENNES_LAYER.roofShape,
+    activeOnStartup: true,
+  })
+  const context = new Context({
+    layers: [roofLayer.toJSON()],
+    viewpoints: [],
+    maps: [olMap],
+    startingViewpointName: 'rennes',
+    startingMapName: 'ol',
+  })
+  const app = new RennesApp(context)
+  app.addContext(context)
+  const wrapper = mount(MapComponent, {
+    global: {
+      plugins: [
+        createTestingPinia({
+          createSpy: vi.fn,
+          stubActions: true,
+          stubPatch: false,
+          fakeApp: true,
+          initialState: {
+            views: {
+              currentView: viewList.home,
+            },
+            address: {
+              latitude: 10,
+              longitude: 10,
+            },
+          },
+        }),
+      ],
+      provide: {
+        rennesApp: app,
+      },
+    },
+  })
+  const roofStore = useRoofsStore()
+
+  it('renders', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  describe('initialize right home interactions', () => {
+    it('start without SelectRoofInteraction on home', () => {
+      app.maps.eventHandler.interactions.forEach((interaction) => {
+        expect(interaction).not.toBeInstanceOf(SelectRoofInteraction)
+      })
+    })
+    it('start without SelectDistrictInteraction on home', () => {
+      app.maps.eventHandler.interactions.forEach((interaction) => {
+        expect(interaction).not.toBeInstanceOf(SelectDistrictInteraction)
+      })
+    })
+    it('start with forbiden click interaction on home', () => {
+      expect(
+        app.maps.eventHandler.interactions.find(
+          (interaction) => interaction instanceof ForbidenClickInteraction
+        )
+      ).toBeTruthy()
+    })
+  })
+
+  describe('setupGridInstallation', async () => {
+    it('set up the interaction, initialize grid, change the zoom, set center', async () => {
+      const geojsonObject: GeoJSONFeatureCollection = {
+        type: 'FeatureCollection',
+        features: [],
+      }
+      roofStore.getFeaturesOfSelectedPanRoof = () => geojsonObject
+      const roofInteractionHelper = await import(
+        '@/services/roofInteractionHelper'
+      )
+      roofInteractionHelper.displayGridOnMap = vi.fn()
+      roofInteractionHelper.displayRoofShape = vi.fn()
+      roofInteractionHelper.generateRectangleGrid = vi
+        .fn()
+        .mockReturnValue({ bbox: [1, 1.5, 1, 2.5] })
+      //roofInteractionHelper.addRoofInteractionOn2dMap = vi.fn()
+
+      await (wrapper.vm as any).setupGridInstallation() //eslint-disable-line
+
+      expect(roofInteractionHelper.displayRoofShape).toHaveBeenCalledOnce()
+      expect(roofInteractionHelper.generateRectangleGrid).toHaveBeenCalledOnce()
+      expect(roofInteractionHelper.generateRectangleGrid).toHaveBeenCalledOnce()
+      expect(
+        roofInteractionHelper.addRoofInteractionOn2dMap
+      ).toHaveBeenCalledOnce()
+      expect(app.getOpenlayerMap().getView().getZoom()).toBe(22)
+      //set center the the middle of the bbox
+      expect(app.getOpenlayerMap().getView().getCenter()![0]).toBe(1)
+      expect(app.getOpenlayerMap().getView().getCenter()![1]).toBe(2)
+    })
+  })
+})
