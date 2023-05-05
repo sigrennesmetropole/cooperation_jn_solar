bbea75b8db944d4f53d9927bb0976765b410830f
Ismail Sunni <imajimatika@gmail.com>
2023-05-05 07:36:23 +0700

    Add unit test for medium and big roof.

--- src/algorithm/__tests__/solarPanelPlacement.spec.ts
+++ src/algorithm/__tests__/solarPanelPlacement.spec.ts
@@ -91,4 +91,38 @@ describe('solar panel placement', () => {
     expect(result.orientation).equal('vertical')
     expect(result.solarPanels.length).equal(3)
   })
+
+  test('solar panel algorithm medium roof', () => {
+    // Read GeoJSON file
+    const fileContents = fs.readFileSync(
+      path.join(__dirname, 'grid_samples', 'medium_roof', 'medium_roof.matrix'),
+      {
+        encoding: 'utf-8',
+      }
+    )
+
+    const matrix: Matrix = JSON.parse(fileContents)
+    expect(matrix.length).greaterThan(0)
+
+    const result = solarPanelPlacementMatrix(matrix, true, 'medium-roof-')
+    expect(result.orientation).equal('vertical')
+    expect(result.solarPanels.length).equal(106)
+  })
+
+  test('solar panel algorithm big roof', () => {
+    // Read GeoJSON file
+    const fileContents = fs.readFileSync(
+      path.join(__dirname, 'grid_samples', 'big_roof', 'big_roof.matrix'),
+      {
+        encoding: 'utf-8',
+      }
+    )
+
+    const matrix: Matrix = JSON.parse(fileContents)
+    expect(matrix.length).greaterThan(0)
+
+    const result = solarPanelPlacementMatrix(matrix, true, 'big-roof-')
+    expect(result.orientation).equal('vertical')
+    expect(result.solarPanels.length).equal(157)
+  })
 })
