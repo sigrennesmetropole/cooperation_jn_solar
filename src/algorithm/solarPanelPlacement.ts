16f088cfd4a45d8d4c016b8f32a59f021bbc863d
Ismail Sunni <imajimatika@gmail.com>
2023-05-08 10:50:00 +0700

    Add necessary comment.

--- src/algorithm/solarPanelPlacement.ts
+++ src/algorithm/solarPanelPlacement.ts
@@ -20,8 +20,11 @@ export type Square = {
 
 export type Matrix = Square[][]
 
+// A grid is a one cell in a matrix. It is represented by its row and column
+export type Grid = [number, number]
+
 // A solar panel representation that show which grid are covered by the solar panel
-export type SolarPanelGrid = [number, number][]
+export type SolarPanelGrid = Grid[]
 
 // Helper function
 
@@ -38,18 +41,22 @@ export function writeFeature(
   }
 }
 
+// Create solar panel from a starting grid (vertical 2x4, horizontal 4x2)
+// Example, from a grid [1, 0] and horizontal, we will get a list of these solar panel
+// [1, 0] [2, 0] [3, 0] [4, 0]
+// [1, 1] [2, 1] [3, 1] [4, 1]
 function createSolarPanelGrid(
-  index: [number, number],
+  startingGrid: Grid,
   horizontal: boolean = true
 ): SolarPanelGrid {
-  const solarPanelGrid: [number, number][] = []
+  const solarPanelGrid: SolarPanelGrid = []
 
   const length = horizontal ? 4 : 2
   const width = horizontal ? 2 : 4
 
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < width; j++) {
-      solarPanelGrid.push([index[0] + i, index[1] + j])
+      solarPanelGrid.push([startingGrid[0] + i, startingGrid[1] + j])
     }
   }
 
@@ -72,6 +79,12 @@ function isAllSolarPanelGridUsable(
   return true
 }
 
+// Check if there is a grid in the solar panel that covers other grid that already covered by other solar panel
+// Example:
+// solarPanelGrid: [1, 0] [2, 0] [3, 0] [4, 0]  [1, 1] [2, 1] [3, 1] [4, 1]
+// coverageMatrix is a matrix where that has true on the cell if the grid is already covered, otherwise false
+// for instance, coverageMatrix of 4x4 matrix with grid [1, 1] [2, 2] [3, 3] are true, and the rest if false
+// In this case, it will return False because the solar panel also has grid [3, 3]
 function isAllSolarPanelGridNotCovered(
   solarPanelGrid: SolarPanelGrid,
   coverageMatrix: number[][]
@@ -165,12 +178,13 @@ export function solarPanelPlacement(
 
 // Used to write a GeoJSON representation of the matrix' to check
 // the order / formation of the grid and the solar panel
+// The representation is a square that has equal length (0.05) and starting from coordinate (0, 0)
 export function matrixToGeoJSON(
   matrix: Matrix,
   solarPanels: SolarPanelGrid[] = []
 ) {
   const fc: FeatureCollection<Polygon, Properties> = featureCollection([])
-  const length = 0.05
+  const length = 0.05 // The length of the square
 
   const coveredGrids: Record<number, number> = {}
   for (let i = 0; i < solarPanels.length; i++) {
@@ -181,6 +195,10 @@ export function matrixToGeoJSON(
 
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[0].length; j++) {
+      // Construct the rectangle.
+      // It needs to replicate the order of the matrix of grid given by Turf.js which the center is 0, 0,
+      // then it goes to the left first (that's why the x is decreased by -length)
+      // after that if fill the next row
       const x: number = i * -length
       const y: number = j * length
       const solarPanelIndex = coveredGrids[j * matrix.length + i]
@@ -198,6 +216,8 @@ export function matrixToGeoJSON(
           index: j * matrix.length + i,
           usable: matrix[i][j].usable,
           solar_panel_index: solarPanelIndex,
+          column: i,
+          row: j,
         }
       )
       fc.features.push(grid)
