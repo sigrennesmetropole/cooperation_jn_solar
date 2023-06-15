dee69e1fdb58c4e26e308d1d589a5a0b900c99e0
Toni GARCIA <toni.garcia.yt@gmail.com>
2023-06-15 17:08:26 +0200

    click building empty display error

---
+++ src/components/roof_selection/NoRoofOnBuilding.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import noSolarPanel from '@/assets/illustrations/no-solar-panel.svg'
+</script>
+
+<template>
+  <div class="rounded-lg bg-slate-100 h-fit mt-20 w-full pt-6 pb-8">
+    <div class="flex flex-col justify-center items-center gap-6">
+      <img class="w-[354px] h-[161px] mx-6" :src="noSolarPanel" alt="" />
+      <span class="font-dm-sans font-normal text-sm text-center px">
+        Sur ce bâtiment, aucun pan de toit ne dispose d'une surface favorable
+        pour accueillir une installation photovoltaïque.
+      </span>
+    </div>
+  </div>
+</template>
--- src/components/roof_selection/SurfaceNumber.vue
+++ src/components/roof_selection/SurfaceNumber.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
 import SurfaceChartDonut from '@/components/chart/SurfaceChartDonut.vue'
 import UiSurfaceNumber from '@/components/roof_selection/UiSurfaceNumber.vue'
-import { ref } from 'vue'
-import { getDataBuilding } from '@/model/roof.model'
-import { useRoofsStore } from '@/stores/roof'
+import type { BuildingSurfaceModel } from '@/model/roof.model'
 
-const roofsStore = useRoofsStore()
-const buildingData = ref(getDataBuilding())
-
-roofsStore.$subscribe(async () => {
-  buildingData.value = getDataBuilding()
-})
+const props = defineProps<{
+  buildingData: BuildingSurfaceModel
+}>()
 </script>
 
 <template>
   <div class="flex flex-row items-center justify-center">
-    <SurfaceChartDonut :buildingData="buildingData"></SurfaceChartDonut>
+    <SurfaceChartDonut :buildingData="props.buildingData"></SurfaceChartDonut>
   </div>
   <UiSurfaceNumber
     :totalArea="buildingData.total"
